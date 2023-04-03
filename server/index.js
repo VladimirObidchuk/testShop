aa76fbc67facfaa75ac3a43c354644ce3f2e2587
Anatoly Kalyuzhny <anatoly.kalyuzhny@gmail.com>
2023-04-03 10:33:35 +0300

    Add docker configuration

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
-const models = require("./models/models");
+const models = require("./src/models/models");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const PORT = process.env.PORT || 5000;
-const router = require("./routes/index");
-const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const router = require("./src/routes/index");
+const errorHandler = require("./src/middleware/ErrorHandlingMiddleware");
 const path = require("path");
 
 const app = express();
